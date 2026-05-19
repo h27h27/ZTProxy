a40556489fb97357c5925640dd670c6eee24c22d
h27h27 <73617049+h27h27@users.noreply.github.com>
2026-05-20 00:09:02 +0330

    Change MainActivity class to public

--- app/src/main/java/com/h27h27/ztproxy/MainActivity.kt
+++ app/src/main/java/com/h27h27/ztproxy/MainActivity.kt
@@ -7,20 +7,17 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() {
+public class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         // Start the SOCKS proxy service
         val i = Intent(this, SocksProxyService::class.java)
         startForegroundService(i)
-
         // Start libzt manager (initialization and join network)
         CoroutineScope(Dispatchers.IO).launch {
             LibZtManager.initialize(applicationContext)
             // TODO: join network and configure virtual interface
         }
-
         finish()
     }
 }
